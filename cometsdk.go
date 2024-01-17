40b9cf7ad0e6731c614dc3c74c91ef55e4d546c1
Peter Ashford <pashford@cometbackup.com>
2024-01-17 16:29:48 +1300

    comet: sync with comet 23.12.3

--- cometsdk.go
+++ cometsdk.go
@@ -16,10 +16,10 @@ import (
 // CONSTANTS
 //
 
-const APPLICATION_VERSION string = "23.12.1"
+const APPLICATION_VERSION string = "23.12.3"
 const APPLICATION_VERSION_MAJOR int = 23
 const APPLICATION_VERSION_MINOR int = 12
-const APPLICATION_VERSION_REVISION int = 1
+const APPLICATION_VERSION_REVISION int = 3
 
 // AutoRetentionLevel: The system will automatically choose how often to run an automatic Retention
 // Pass after each backup job.
